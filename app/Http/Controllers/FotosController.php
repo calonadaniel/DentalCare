ac23c04bdc1713e0ad23a04172ed3e308669e158
calonadaniel <57235682+calonadaniel@users.noreply.github.com>
2020-04-28 23:59:35 -0600

    test fix gallery not creating folder on the fly for files

--- app/Http/Controllers/FotosController.php
+++ app/Http/Controllers/FotosController.php
@@ -54,9 +54,6 @@ class FotosController extends Controller
         /*if (!file_exists($path)) {
             mkdir($path, 0777, true);
         } */
-        if (!File::exists($path)) {
-            File::makeDirectory($path, 0775, true, true);
-        }
 
         $width = 1280; // your max width
         $height = 1280; // your max height
