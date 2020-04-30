ed8e5735d6593c988dea5cfa70c8229018826240
calonadaniel <57235682+calonadaniel@users.noreply.github.com>
2020-04-30 12:17:51 -0600

    search bar minor fix test

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,4 +1,4 @@
 <div class="row my-3 d-flex justify-content-center active-search">
     <input class="w-10 active-line text-center" style="border:none; outline:none" type="text" id="myInput" onkeyup="myFunction()" placeholder="{{$placeholder}}" >
-    <i class="fas fa-search fa-2x " aria-hidden="true"></i>
+    <i class="fas fa-search fa-2x text-center" aria-hidden="true"></i>
 </div>
